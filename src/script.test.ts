29be2a707c5f4f87d9ea04fcf7a82dcb6f83343b
Filip Tarajko <76916685+FilipTarajko@users.noreply.github.com>
2025-04-30 22:18:15 +0200

    Add nextStart checks to textToSpans tests

--- src/script.test.ts
+++ src/script.test.ts
@@ -12,11 +12,11 @@ describe('textToWords', () => {
 })
 
 describe('textToSpans', () => {
-    it('transforms a word into a span', () => {
-        expect(textToSpans('test', ['test'], 0, 0)[0]).toBe("<span id=\'0-0\'>test</span>");
+    it('transforms a word into a span and knows where next replaceable stuff may begin', () => {
+        expect(textToSpans('test', ['test'], 0, 0)).toStrictEqual(["<span id=\'0-0\'>test</span>", 27]);
     })
 
-    it('transforms a string with multiple words into spans', () => {
-        expect(textToSpans('test a abc', ['test', 'a', 'abc'], 0, 0)[0]).toBe("<span id='0-0'>test</span> <span id='0-1'>a</span> <span id='0-2'>abc</span>");
+    it('transforms a string with multiple words into spans and knows where next replaceable stuff may begin', () => {
+        expect(textToSpans('test a abc', ['test', 'a', 'abc'], 0, 0)).toStrictEqual(["<span id='0-0'>test</span> <span id='0-1'>a</span> <span id='0-2'>abc</span>", 77]);
     })
 })
